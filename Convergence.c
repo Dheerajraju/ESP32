422011b9710e39b965209d54c84451e4c9444c91
Dheeraj raju <dheshal07@gmail.com>
2026-05-28 14:30:24 +0530

    Update Convergence.c

--- Convergence.c
+++ Convergence.c
@@ -1,37 +1,31 @@
-#define LED1 4
-#define LED2 5
-#define LED3 6
-#define LED4 7
-#define LED5 0
-#define LED6 1
+int leds[] = {4, 5, 6, 7, 0, 1};
+int numLeds = 6;
 
 void setup()
 {
-  pinMode(LED1,OUTPUT);
-  pinMode(LED2,OUTPUT);
-  pinMode(LED3,OUTPUT);
-  pinMode(LED4,OUTPUT);
-  pinMode(LED5,OUTPUT);
-  pinMode(LED6,OUTPUT);
+  for(int i = 0; i < numLeds; i++)
+  {
+    pinMode(leds[i], OUTPUT);
+  }
 }
 
 void loop()
 {
-  digitalWrite(LED1,HIGH);
-  digitalWrite(LED6,HIGH);
-  delay(500);
-  digitalWrite(LED2,HIGH);
-  digitalWrite(LED5,HIGH);
-  delay(500);
-  digitalWrite(LED3,HIGH);
-  digitalWrite(LED4,HIGH);
-  delay(500);
-  digitalWrite(LED1,LOW);
-  digitalWrite(LED6,LOW);
-  delay(500);
-  digitalWrite(LED2,LOW);
-  digitalWrite(LED5,LOW);
-  delay(500);
-  digitalWrite(LED3,LOW);
-  digitalWrite(LED4,LOW);
+  // Turn ON from outside to center
+  for(int i = 0; i < numLeds / 2; i++)
+  {
+    digitalWrite(leds[i], HIGH);
+    digitalWrite(leds[numLeds - 1 - i], HIGH);
+
+    delay(500);
+  }
+
+  // Turn OFF from outside to center
+  for(int i = 0; i < numLeds / 2; i++)
+  {
+    digitalWrite(leds[i], LOW);
+    digitalWrite(leds[numLeds - 1 - i], LOW);
+
+    delay(500);
+  }
 }
